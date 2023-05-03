08b8c904add80879bac4d80a2a50d12964ff75c4
mt_tahmid <md.tasin.tahmid1998@gmail.com>
2023-05-03 15:35:31 +0600

    third commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./scenes/homePage";
+import LoginPage from "./scenes/loginPage";
+import ProfilePage from "./scenes/profilePage";
 
 
 function App() {
   return (
     <div className="app">
-      
+      <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
     </div>
   );
 }
