21bdf7e3eca48b17503c3b3f72eb040622f4c4f5
mt_tahmid <md.tasin.tahmid1998@gmail.com>
2023-05-30 19:35:26 +0600

    fifth commit

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,10 @@
 import express from "express";
+import { verifyToken } from "../middlewares/auth.js";
 import {
   getUser,
   getUserFriends,
   addRemoveFriend,
 } from "../controllers/users.js";
-import { verifyToken } from "../middlewares/auth.js";
 
 const router = express.Router();
 
