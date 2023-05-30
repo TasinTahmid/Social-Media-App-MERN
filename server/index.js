ecf76b884909b0746a15fd0703ead10ca85da7ee
mt_tahmid <md.tasin.tahmid1998@gmail.com>
2023-05-30 19:38:10 +0600

    sixth commit

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import authRoutes from "./routes/auth.js";
-import userRoutes from "./routes/users.js"
 import postRoutes from "./routes/posts.js"
+import userRoutes from "./routes/users.js"
 import { verifyToken } from "./middlewares/auth.js";
 
 const __filename = fileURLToPath(import.meta.url);
