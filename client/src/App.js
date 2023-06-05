2423508329d099e080a09fa90f8997bbe2991705
mt_tahmid <md.tasin.tahmid1998@gmail.com>
2023-06-05 10:37:37 +0600

    ninth commit

--- client/src/App.js
+++ client/src/App.js
@@ -20,9 +20,9 @@ function App() {
         <ThemeProvider theme = { theme }>
           <CssBaseline />
           <Routes>
-            <Route path="/" element={<LoginPage />} />
+            {/* <Route path="/" element={<LoginPage />} /> */}
             <Route
-              path="/home" element={ <HomePage/>}
+              path="/" element={ <HomePage/>}
             />
             <Route
               path="/profile/:userId" element={<ProfilePage />}
--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../navbar';
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <Navbar />
   )
 }
 
