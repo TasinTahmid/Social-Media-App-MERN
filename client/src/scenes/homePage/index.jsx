7f3ad27c5ada239ad7b372d6aef95d5d793b156e
mt_tahmid <md.tasin.tahmid1998@gmail.com>
2023-06-09 16:27:43 +0600

    fourteenth commit

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
 import Navbar from '../navbar';
+import UserWidget from "../../scenes/widgets/UserWidget";
+import MyPostWidget from "../../scenes/widgets/MyPostWidget";
 
 function HomePage() {
+  const isNonMobileScreens = useMediaQuery("(min-width:850px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
   return (
-    <Navbar />
+    <Box>
+      <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath} />
+          {/* <PostsWidget userId={_id} /> */}
+        </Box>
+
+      </Box>
+
+    </Box>
   )
 }
 
